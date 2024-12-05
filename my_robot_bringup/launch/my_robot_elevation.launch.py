f5bb777a71c27d4530bba1fbb3c3078874322687
papitogalactic <juanrapf00@gmail.com>
2024-12-05 12:07:31 +0100

    cambiosbringup
    Signed-off-by: papitogalactic <juanrapf00@gmail.com>

--- my_robot_bringup/launch/my_robot_elevation.launch.py
+++ my_robot_bringup/launch/my_robot_elevation.launch.py
@@ -8,15 +8,19 @@ from launch.actions import DeclareLaunchArgument
 from ament_index_python import get_package_share_directory
 import os
 
+
+mvsimDir = get_package_share_directory("mvsim")
 pkg_path = os.path.join(get_package_share_directory('my_robot_bringup'))
 
 MVSIM_WORLD_FILE = os.path.join(pkg_path, 'worlds','demo_elevation_map.world.xml')
-MVSIM_ROS2_PARAMS_FILE = os.path.join(pkg_path, 'config','mvsim_ros2_params.yaml')
-RVIZ2_FILE = os.path.join(pkg_path, 'rviz','demo_depth_camera_ros2.rviz')
+MVSIM_ROS2_PARAMS_FILE = os.path.join(mvsimDir, 'mvsim_tutorial',
+                                      'mvsim_ros2_params.yaml')
+RVIZ2_FILE = os.path.join(mvsimDir, 'mvsim_tutorial',
+                          'demo_depth_camera_ros2.rviz')
 
 
 def generate_launch_description():
-    # args that can be set from the command line or a default will be used
+    # args that can be set from the command line or a default will be used a
     world_file_launch_arg = DeclareLaunchArgument(
         "world_file", default_value=TextSubstitution(
             text=MVSIM_WORLD_FILE))
